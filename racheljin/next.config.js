207824f9353370563dc7d13c58840efe8fe6bf67
Rachel Jin <racheljin24@gmail.com>
2024-08-01 20:28:41 -0400

    try5

--- racheljin/next.config.js
+++ racheljin/next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/racheljin" : "",
-  images: {
-    unoptimized: true,
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   basePath: process.env.NODE_ENV === "production" ? "/racheljin" : "",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
