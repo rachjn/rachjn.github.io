64717a45672623585598bb2c8d96c98d6ecffc09
Rachel Jin <racheljin24@gmail.com>
2024-08-01 20:27:25 -0400

    try5

--- racheljin/next.config.mjs
+++ racheljin/next.config.mjs
@@ -1,24 +1,35 @@
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   basePath: "/personal-site",
+// //   output: "export",
+// //   reactStrictMode: true,
+// // };
+
+// // export default nextConfig;
+
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   basePath: "/2048-in-react",
+// //   output: "export",
+// //   reactStrictMode: true,
+// // };
+
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   basePath: "/personal-site",
 //   output: "export",
-//   reactStrictMode: true,
 // };
 
 // export default nextConfig;
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   basePath: "/2048-in-react",
-//   output: "export",
-//   reactStrictMode: true,
-// };
+// // module.exports = nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/racheljin" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
-
-// module.exports = nextConfig;
