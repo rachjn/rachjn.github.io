1367c55234b8a3d6e4a177603c6aba7783fedd11
Rachel Jin <racheljin24@gmail.com>
2024-08-01 20:15:08 -0400

    try3

---
+++ racheljin/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/racheljin",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
--- racheljin/next.config.mjs
+++ racheljin/next.config.mjs
@@ -6,3 +6,12 @@
 // };
 
 // export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/2048-in-react",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
